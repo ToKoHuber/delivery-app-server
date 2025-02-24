c0779a5670928992e37a077a838637ea72a13cd3
24LP2879 24LP2879 <24LP2879@admins-iMac.local>
2025-02-24 13:20:28 +0800

    push

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -1,8 +1,10 @@
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
+const { MongoClient } = require("mongodb");
+import { configDotenv } from "dotenv";
 
-export async function getMovies(year: number, imdb: number) {
+export async function getMovies(year: number, rating: number) {
   configDotenv();
-  const uri = process.env.MONGODB_URL || "";
+  const uri = process.env.MONGODB_URL;
   const client = new MongoClient(uri);
   console.log("uri", uri);
 
@@ -12,12 +14,12 @@ export async function getMovies(year: number, imdb: number) {
     const collection = database.collection("movies");
 
     const findQuery = {
-      $and: [{ "imdb.rating": { $gt: imdb } }, { year: { $gt: year } }],
+      $and: [{ "imdb.rating": { $gt: rating } }, { year: { $gt: year } }],
     };
     const movies = await collection.find(findQuery).limit(10).toArray();
     return movies;
-  } catch (e) {
-    console.log(e);
+  } catch (err) {
+    console.error(`Something went wrong trying to delete documents: ${err}\n`);
   } finally {
     await client.close();
   }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { getMovies } from "./database/mongodb";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+require("dotenv").config({
+  path: "/Users/24LP2879/Desktop/Pinecone/delivery-app/server/.env",
+});
+
+const port = 4000;
 
 // app.use("/food", foodRoute);
 // app.use("/food-category", categoryRoute);
@@ -13,14 +19,14 @@ app.use(cors());
 
 app.get("movies", async (req, res) => {
   try {
-    const { year, imdb } = await req.query;
-    const movies = await getMovies(Number(year), Number(imdb));
+    const { year, rating } = await req.query;
+    const movies = await getMovies(Number(year), Number(rating));
     res.status(200).json({ message: "success", movies: movies });
   } catch (error) {
     res.status(500).json({ message: "error", error });
   }
 });
 
-app.listen(4000, () => {
-  console.log(`listening on port ${4000}`);
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });

b4cc033c81fcbda31da8a16168cbcda3ea5eabe4
Your Name <24LP2879@admins-iMac.local>
2025-03-19 13:16:35 +0800

    added

---
+++ src/controllers/forget-password.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import userModel from "../models/user.model";
+import { sendEmail } from "../util/send-email";
+
+const jwtSecret = process.env.JWT_SECRET;
+
+export const forgetPassword = async (req: Request, res: Response) => {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      res.status(401).json({ messsage: "Emailee oruulna uu" });
+      return;
+    }
+
+    const user = await userModel.findOne({ email });
+    console.log("user", user);
+
+    if (!user) {
+      res.status(401).json({ messsage: "Burtgeltei hereglegch alga" });
+      return;
+    }
+
+    console.log("jwt-secret", jwtSecret);
+    //http://localhost:3000/reset-password?id=kl12k32193
+
+    const token = jwt.sign({ id: user._id }, jwtSecret!, { expiresIn: "1h" });
+
+    await sendEmail(email, token);
+    res.status(200).json({ message: "amjilttai" });
+  } catch (error) {
+    res.status(500).json({ message: "Aldaa garlaa", error });
+  }
+};
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { signUp } from "../controllers/auth/signUp.controller";
 import { signIn } from "../controllers/auth/signIn.controller";
+import { forgetPassword } from "../controllers/forget-password.controller";
+import { resetPassword } from "../controllers/reset-password.controller";
 
 const authRouth = Router();
 
 authRouth.post("/signin", signIn);
 authRouth.post("/signup", signUp);
 // authRouth.post("/sign-in", UserInfo);
-// authRouth.post("/reset-password-request", UserInfo);
+authRouth.post("/reset-password-request", forgetPassword);
 // authRouth.get("/verify-reset-password-request", UserInfo);
-// authRouth.post("/reset-password", UpdateProfile);
+authRouth.post("/reset-password", resetPassword);
 export { authRouth };
